2d19241da979700815e81ec2c4275d2f709ac035
Steve Albonico <36756929+salbonico@users.noreply.github.com>
2019-07-12 14:42:27 -0700

    Added Our Team section

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,6 +50,125 @@ const IndexPage = () => (
 </div>
 </section>
 
+<div class="section">
+<div class="container clearfix">
+<div class="heading-block topmargin-sm center">
+<h3>Our Team</h3>
+<span class="divcenter">People who have contributed enormously to our Company</span>
+</div>
+<div className="row">
+<div className="col-lg-6 bottommargin">
+<div className="team team-list clearfix">
+<div className="team-image">
+<img src="http://themes.semicolonweb.com/html/canvas/images/team/3.jpg" alt="John Doe" />
+</div>
+<div className="team-desc">
+<div className="team-title"><h4>John Doe</h4><span>CEO</span></div>
+<div className="team-content">
+<p>Carbon emissions reductions giving, legitimize amplify non-partisan Aga Khan. Policy dialogue assessment expert free-speech cornerstone disruptor freedom. Cesar Chavez empower.</p>
+</div>
+<a href="#" className="social-icon si-rounded si-small si-facebook">
+<i className="icon-facebook"></i>
+<i className="icon-facebook"></i>
+</a>
+<a href="#" className="social-icon si-rounded si-small si-twitter">
+<i className="icon-twitter"></i>
+<i className="icon-twitter"></i>
+</a>
+<a href="#" className="social-icon si-rounded si-small si-gplus">
+<i className="icon-gplus"></i>
+<i className="icon-gplus"></i>
+</a>
+</div>
+</div>
+</div>
+<div className="col-lg-6 bottommargin">
+<div className="team team-list clearfix">
+<div className="team-image">
+<img src="http://themes.semicolonweb.com/html/canvas/images/team/2.jpg" alt="Nix Maxwell" />
+</div>
+<div className="team-desc">
+<div className="team-title"><h4>Nix Maxwell</h4><span>Support</span></div>
+<div className="team-content">
+<p>Eradicate invest honesty human rights accessibility theory of social change. Diversity experience in the field compassion, inspire breakthroughs planned giving.</p>
+</div>
+<a href="#" className="social-icon si-rounded si-small si-forrst">
+<i className="icon-forrst"></i>
+<i className="icon-forrst"></i>
+</a>
+<a href="#" className="social-icon si-rounded si-small si-skype">
+<i className="icon-skype"></i>
+<i className="icon-skype"></i>
+</a>
+<a href="#" className="social-icon si-rounded si-small si-flickr">
+<i className="icon-flickr"></i>
+<i className="icon-flickr"></i>
+</a>
+</div>
+</div>
+</div>
+</div>
+
+<div className="row">
+<div className="col-lg-6 bottommargin">
+<div className="team team-list clearfix">
+<div className="team-image">
+<img src="http://themes.semicolonweb.com/html/canvas/images/team/4.jpg" alt="John Doe" />
+</div>
+<div className="team-desc">
+<div className="team-title"><h4>Jon Tableman</h4><span>CEO</span></div>
+<div className="team-content">
+<p>Carbon emissions reductions giving, legitimize amplify non-partisan Aga Khan. Policy dialogue assessment expert free-speech cornerstone disruptor freedom. Cesar Chavez empower.</p>
+</div>
+<a href="#" className="social-icon si-rounded si-small si-facebook">
+<i className="icon-facebook"></i>
+<i className="icon-facebook"></i>
+</a>
+<a href="#" className="social-icon si-rounded si-small si-twitter">
+<i className="icon-twitter"></i>
+<i className="icon-twitter"></i>
+</a>
+<a href="#" className="social-icon si-rounded si-small si-gplus">
+<i className="icon-gplus"></i>
+<i className="icon-gplus"></i>
+</a>
+</div>
+</div>
+</div>
+<div className="col-lg-6 bottommargin">
+<div className="team team-list clearfix">
+<div className="team-image">
+<img src="http://themes.semicolonweb.com/html/canvas/images/team/8.jpg" alt="Nix Maxwell" />
+</div>
+<div className="team-desc">
+<div className="team-title"><h4>Mary Jane</h4><span>Support</span></div>
+<div className="team-content">
+<p>Eradicate invest honesty human rights accessibility theory of social change. Diversity experience in the field compassion, inspire breakthroughs planned giving.</p>
+</div>
+<a href="#" className="social-icon si-rounded si-small si-forrst">
+<i className="icon-forrst"></i>
+<i className="icon-forrst"></i>
+</a>
+<a href="#" className="social-icon si-rounded si-small si-skype">
+<i className="icon-skype"></i>
+<i className="icon-skype"></i>
+</a>
+<a href="#" className="social-icon si-rounded si-small si-flickr">
+<i className="icon-flickr"></i>
+<i className="icon-flickr"></i>
+</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+
+
 <div id="gotoTop" className="icon-angle-up"></div>
 
 
