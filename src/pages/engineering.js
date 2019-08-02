d52a11202c6b4f3474aa83f6d456cc9fd5bdd669
derek <dtstegall@gmail.com>
2019-08-01 17:42:44 -0700

    Updated the photos + bio paragraphs on the engineering page.

--- src/pages/engineering.js
+++ src/pages/engineering.js
@@ -12,7 +12,7 @@ const SoftwareEngineeringTeam = () => (
     <section id="page-title">
       <div className="container clearfix">
         <h1>Software Engineering Team</h1>
-        <span>List of People who matter in Our Company</span>
+        <span>List of Software Engineers</span>
         <ol className="breadcrumb">
           <li className="breadcrumb-item">
             <a href="/">Home</a>
@@ -33,148 +33,185 @@ const SoftwareEngineeringTeam = () => (
             </div>
           </Col>
           <Col lg="3" md="6" className="bottommargin">
+
             <div className="team">
+
               <div className="team-image">
                 <img
-                  src="https://d1xrp9zhb3ks3c.cloudfront.net/web/antioch-toastmasters/template/images/team/3.jpg"
-                  alt="John Doe"
+                  src="https://d1xrp9zhb3ks3c.cloudfront.net/web/ortabrothers/images/2018/software-engineers-isaiah-orta.jpg"
+                  alt="Isaiah Orta"
                   className="m-0"
                 />
               </div>
+
               <div className="team-desc">
                 <div className="team-title">
-                  <h4>John Doe</h4>
-                  <span>CEO</span>
+                  <h4>Isaiah Orta</h4>
+                  <span>Chief Executive Officer</span>
                 </div>
               </div>
+
+              <div class="caption text-left">
+                <p>Isaiah is responsible for the the growth, stability, direction and daily operation of Orta Brothers.</p>
+                <a href="https://www.linkedin.com/in/isaias-orta-0992b783/" class="social rounded fa fa-linkedin" target="_blank" rel="noopener noreferrer"></a>
+                <a href="https://www.ortabrothers.com" class="social rounded fa fa-code" target="_blank" rel="noopener noreferrer"></a>
+              </div>
+
             </div>
+
           </Col>
 
           <Col lg="3" md="6" className="bottommargin">
+
             <div className="team">
+              
               <div className="team-image">
                 <img
-                  src="https://d1xrp9zhb3ks3c.cloudfront.net/web/antioch-toastmasters/template/images/team/2.jpg"
-                  alt="Josh Clark"
+                  src="https://d1xrp9zhb3ks3c.cloudfront.net/web/chateaujudsonville/images/profile-duaneleem.jpg"
+                  alt="Duane Leem"
                   className="m-0"
                 />
               </div>
+
               <div className="team-desc">
                 <div className="team-title">
-                  <h4>John Doe</h4>
-                  <span>Co-Founder</span>
+                  <h4>Duane Leem</h4>
+                  <span>Project Manager</span>
                 </div>
               </div>
+
+              <div class="caption text-left">
+                <p>With extensive knowledge in the IT Field and Project Management Duane is able to lead his team to complete quality work and designs for his clients, Duane is a veteran in the field. While using web development to solve problems and in turn creating solutions. Duane is an avid consumer of knowledge while passing his passion to his Children and those around him.</p>
+                <a href="https://www.facebook.com/duaneleem" class="social rounded fa fa-facebook" target="_blank" rel="noopener noreferrer"></a>
+                <a href="https://www.linkedin.com/in/duaneleem" class="social rounded fa fa-linkedin" target="_blank" rel="noopener noreferrer"></a>
+                <a href="https://duaneleem.com/" class="social rounded fa fa-code" target="_blank" rel="noopener noreferrer"></a>
+              </div>
+
             </div>
+
           </Col>
 
           <Col lg="3" md="6" className="bottommargin">
+
             <div className="team">
+              
               <div className="team-image">
                 <img
-                  src="https://d1xrp9zhb3ks3c.cloudfront.net/web/antioch-toastmasters/template/images/team/8.jpg"
-                  alt="Mary Jane"
+                  src="https://d1xrp9zhb3ks3c.cloudfront.net/web/chateaujudsonville/images/profile-gabetumbaga.jpg"
+                  alt="Gabe Tumbaga"
                   className="m-0"
                 />
               </div>
+
               <div className="team-desc">
                 <div className="team-title">
-                  <h4>Jane Doe</h4>
-                  <span>Sales</span>
+                  <h4>Gabe Tumbaga</h4>
+                  <span>React Engineer</span>
                 </div>
               </div>
+
+              <div class="caption text-left">
+                <p>With experience from multiple aspects in the field Gabe has been apart of a lot. whether its upkeep on ecommerce, React or the many other programming languages he knows. when he's not with his kids or family, you can find him modding Minecraft and tweaking his other favorite games.</p>
+                <a href="https://facebook.com/gtumbaga" class="social rounded fa fa-facebook" target="_blank" rel="noopener noreferrer"></a>
+                <a href="https://www.linkedin.com/in/gtumbaga/" class="social rounded fa fa-linkedin" target="_blank" rel="noopener noreferrer"></a>
+                <a href="https://omga.be/" class="social rounded fa fa-code" target="_blank" rel="noopener noreferrer"></a>
+              </div>
+
             </div>
+
+          </Col>
+          
+          <Col lg="3" md="6" className="bottommargin">
+
+            <div className="team">
+              
+              <div className="team-image">
+                <img
+                  src="https://d1xrp9zhb3ks3c.cloudfront.net/web/chateaujudsonville/images/profile-derrickleem.jpg"
+                  alt="Derrick Leem"
+                  className="m-0"
+                />
+              </div>
+
+              <div className="team-desc">
+                <div className="team-title">
+                  <h4>Derrick Leem</h4>
+                  <span>Front-End Engineer</span>
+                </div>
+              </div>
+
+              <div class="caption text-left">
+                <p>Derrick while doing web development is also currently in school pursuing a degree in computer science. While applying the skills he already had coupled with the new skills through study has propelled him to his current skill level.</p>
+                <a href="https://www.linkedin.com/in/derrick-leem/" class="social rounded fa fa-linkedin" target="_blank" rel="noopener noreferrer"></a>
+              </div>
+
+            </div>
+
           </Col>
 
           <Col lg="3" md="6" className="bottommargin">
+
             <div className="team">
-              <div className="team-image">
-                <img
-                  src="https://d1xrp9zhb3ks3c.cloudfront.net/web/antioch-toastmasters/template/images/team/4.jpg"
-                  alt="Nix Maxwell"
-                  className="m-0"
-                />
-              </div>
-              <div className="team-desc">
-                <div className="team-title">
-                  <h4>John Doe</h4>
-                  <span>Support</span>
-                </div>
+
+            <div className="team-image">
+              <img
+              src="https://d1xrp9zhb3ks3c.cloudfront.net/web/ortabrothers/images/profile-carlo-antonio-antonio-bilbao.jpg"
+              alt="Derrick Leem"
+              className="m-0"
+              />
+            </div>
+
+            <div className="team-desc">
+              <div className="team-title">
+                <h4>Carlo Anthony</h4>
+                <span>React Engineer</span>
               </div>
             </div>
+
+            <div class="caption text-left">
+              <p>I am both a software developer and biotech professional based in the San Francisco Bay Area. I'm passionate about using current and emerging technologies to help solve old problems. Specifically, problems that plague those that are less fortunate, marginalized, or in situations where they are not able to fix their predicament. It’s these problems and the avenues that new technology creates, that motivates me to learn and develop new skills and technologies.</p>
+              <a href="https://www.linkedin.com/in/carlo-bilbao/" class="social rounded fa fa-linkedin" target="_blank" rel="noopener noreferrer"></a>
+            </div>
+
+            </div>
+
           </Col>
 
-          <Col lg="3" md="6" className="bottommargin">
-            <div className="team">
-              <div className="team-image">
-                <img
-                  src="https://d1xrp9zhb3ks3c.cloudfront.net/web/antioch-toastmasters/template/images/team/3.jpg"
-                  alt="Nix Maxwell"
-                  className="m-0"
-                />
-              </div>
-              <div className="team-desc">
-                <div className="team-title">
-                  <h4>John Doe</h4>
-                  <span>Support</span>
-                </div>
-              </div>
-            </div>
-          </Col>
 
           <Col lg="3" md="6" className="bottommargin">
+
             <div className="team">
-              <div className="team-image">
-                <img
-                  src="https://d1xrp9zhb3ks3c.cloudfront.net/web/antioch-toastmasters/template/images/team/2.jpg"
-                  alt="Nix Maxwell"
-                  className="m-0"
-                />
-              </div>
-              <div className="team-desc">
-                <div className="team-title">
-                  <h4>John Doe</h4>
-                  <span>Support</span>
-                </div>
+
+            <div className="team-image">
+              <img
+              src="https://d1xrp9zhb3ks3c.cloudfront.net/web/chateaujudsonville/images/profile-willcarpio.jpg"
+              alt="Will Carpio"
+              className="m-0"
+              />
+            </div>
+
+            <div className="team-desc">
+              <div className="team-title">
+                <h4>Will Carpio</h4>
+                <span>SEO Analyst</span>
               </div>
             </div>
+
+            <div class="caption text-left">
+              <p>New to the development team but on the fast track to becoming on par with his peers, Will is versed and knowledgeable in a few languages and on the way to mastering more. When not down working on new project you can usually find him with his dog Bart.</p>
+              <a href="https://www.facebook.com/people/Will-Carpio/100008976306275" class="social rounded fa fa-facebook" target="_blank" rel="noopener noreferrer"></a>
+              <a href="https://www.linkedin.com/in/will-carpio-821b1815a/" class="social rounded fa fa-linkedin" target="_blank" rel="noopener noreferrer"></a>
+              <a href="https://twitter.com/Humble_Gatsby" class="social rounded fa fa-twitter" target="_blank" rel="noopener noreferrer"></a>
+            </div>
+
+            </div>
+
           </Col>
 
-          <Col lg="3" md="6" className="bottommargin">
-            <div className="team">
-              <div className="team-image">
-                <img
-                  src="https://d1xrp9zhb3ks3c.cloudfront.net/web/antioch-toastmasters/template/images/team/8.jpg"
-                  alt="Nix Maxwell"
-                  className="m-0"
-                />
-              </div>
-              <div className="team-desc">
-                <div className="team-title">
-                  <h4>Jane Doe</h4>
-                  <span>Support</span>
-                </div>
-              </div>
-            </div>
-          </Col>
+         
 
-          <Col lg="3" md="6" className="bottommargin">
-            <div className="team">
-              <div className="team-image">
-                <img
-                  src="https://d1xrp9zhb3ks3c.cloudfront.net/web/antioch-toastmasters/template/images/team/4.jpg"
-                  alt="Nix Maxwell"
-                  className="m-0"
-                />
-              </div>
-              <div className="team-desc">
-                <div className="team-title">
-                  <h4>John Doe</h4>
-                  <span>Support</span>
-                </div>
-              </div>
-            </div>
-          </Col>
+
+
         </Row>
       </Container>
     </div>
