b07bb5a6d8daf9f57f858f5e92fb51f624a01b08
Derrick Leem <derricktleem@gmail.com>
2019-08-27 08:54:24 -0700

    Added profile pics

--- src/pages/index.js
+++ src/pages/index.js
@@ -174,13 +174,13 @@ const IndexPage = () => (
             </div>
             <div className="fbox-desc">
               <h3>
-                Why it Works.
-                <span className="subtitle">Make our Customers Happy.</span>
+                How Will You Benefit?
+                <span className="subtitle">Make you a more confident speaker and leader.</span>
               </h3>
               <p>
-                Porro repellat vero sapiente amet vitae quibusdam necessitatibus
-                consectetur, labore totam. Accusamus perspiciatis asperiores
-                labore esse ab accusantium ea modi ut.
+                Attend meetings where you will regularly practice skills that make you a more confident 
+                speaker and leader. These real-world, highly in-demand soft skills will help you advance 
+                your career, acclimate to a new city or even get a new job!
               </p>
             </div>
           </div>
@@ -405,27 +405,17 @@ const IndexPage = () => (
     <div className="promo promo-light" style={{ marginTop: "-100px" }}>
       <div style={{ width: "75%", marginLeft: "10%" }}>
         <h3>
-          Call us today at <span>+91.22.57412541</span> or Email us at{" "}
-          <span>
-            <a
-              href="/cdn-cgi/l/email-protection"
-              className="__cf_email__"
-              datacfemail="62111712120d10162201030c1403114c010d0f"
-            >
-              test@test.com
-            </a>
-          </span>
+          Call Carol at <a href="tel:9257522192">(925) 752-2192</a>
         </h3>
         <span>
-          We strive to provide Our Customers with Top Notch Support to make
-          their Theme Experience Wonderful
+          We would love for you to come and visit our club anytime!
         </span>
         <a
           style={{ marginRight: "10%" }}
-          href="/"
+          href="/contact-us"
           className="button button-xlarge button-rounded"
         >
-          Start Browsing
+          Contact Us
         </a>
       </div>
     </div>
