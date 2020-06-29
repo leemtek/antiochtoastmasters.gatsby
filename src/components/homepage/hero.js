2da9f306bb303c8e00b6085c954111498ac1c691
Duane Leem <duane@leemfamily.com>
2020-06-28 22:29:52 -0700

    Updated Hero with Zoom info

--- src/components/homepage/hero.js
+++ src/components/homepage/hero.js
@@ -1,103 +1,73 @@
 import React from "react";
 
-const Hero = () => {
-  <div>
-    <section
-      id="slider"
-      className="slider-element slider-parallax swiper_wrapper full-screen clearfix"
-    >
-      {/* <!-- First Slider Pic
-      ============================================= --> */}
-      <div className="slider-parallax-inner">
-        <div className="swiper-container swiper-parent">
-          <div className="swiper-wrapper">
-            <div
-              className="swiper-slide dark"
-              style={{
-                background: `linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url(https://d1xrp9zhb3ks3c.cloudfront.net/web/antioch-toastmasters/images/kaiser-antioch.jpg)`
-              }}
-            >
-              <div className="container clearfix">
-                <div className="slider-caption slider-caption-center">
-                  <h2 data-animate="fadeInUp">Welcome to ToastMasters</h2>
-                  <p
-                    className="d-none d-sm-block"
-                    data-animate="fadeInUp"
-                    data-delay="200"
-                  >
-                    Create just what you need for your Perfect Website. Choose
-                    from a wide range of Elements &amp; simply put them on your
-                    own Canvas.
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            {/* <!-- Second Slider Pic
-            ============================================= --> */}
-            <div
-              className="swiper-slide dark"
-              style={{
-                backgroundImage: `url(https://d1xrp9zhb3ks3c.cloudfront.net/web/antioch-toastmasters/template/images/slider/swiper/2.jpg)`,
-              }}
-            >
-              <div className="container clearfix">
-                <div className="slider-caption slider-caption-center">
-                  <h2 data-animate="fadeInUp">Great Performance</h2>
-                  <p
-                    className="d-none d-sm-block"
-                    data-animate="fadeInUp"
-                    data-delay="200"
-                  >
-                    You'll be surprised to see the Final Results of your
-                    Creation &amp; would crave for more.
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            {/* <!-- Third Slider Pic
-            ============================================= --> */}
-            <div
-              className="swiper-slide"
-              style={{
-                backgroundImage: `url(https://d1xrp9zhb3ks3c.cloudfront.net/web/antioch-toastmasters/template/images/slider/swiper/3.jpg)`,
-              }}
-            >
-              <div className="container clearfix">
-                <div className="slider-caption ">
-                  <h2 data-animate="fadeInUp">Great Speaches</h2>
-                  <p
-                    className="d-none d-sm-block"
-                    data-animate="fadeInUp"
-                    data-delay="200"
-                  >
-                    You'll be surprised to see the Final Results of your
-                    Creation &amp; would crave for more.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="slider-arrow-left">
-            <i className="icon-angle-left"></i>
-          </div>
-          <div className="slider-arrow-right">
-            <i className="icon-angle-right"></i>
-          </div>
-        </div>
-        <a
-          href="/"
-          data-scrollto="#content"
-          data-offset="100"
-          className="dark one-page-arrow"
+class Hero extends React.Component {
+  render() {
+    return (
+      <div>
+        <section
+          id="slider"
+          className="slider-element slider-parallax swiper_wrapper full-screen clearfix"
         >
-          <i className="icon-angle-down infinite animated fadeInDown"></i>
-        </a>
-      </div>
-    </section>
+          {/* <!-- First Slider Pic
+          ============================================= --> */}
+          <div className="slider-parallax-inner">
+            <div className="swiper-container swiper-parent">
+              <div className="swiper-wrapper">
+                <div
+                  className="swiper-slide dark"
+                  style={{
+                    background: `linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url(https://d1xrp9zhb3ks3c.cloudfront.net/web/antioch-toastmasters/images/kaiser-antioch.jpg)`
+                  }}
+                >
+                  <div className="container clearfix">
+                    <div className="row">
+                      <div className="col-md-12 col-lg-3 slider-caption">
+                        <img src="https://d1xrp9zhb3ks3c.cloudfront.net/web/antioch-toastmasters/images/toastmasters-international-icon.png" />
+                      </div>{/* /col */}
+                      <div className="col-md-12 col-lg-9 slider-caption">
+                        <div>Welcome to</div>
+                          <h2 data-animate="fadeInUp" style={{fontSize: "1.3em"}}>Creating Communicators</h2>
+                          <p className="d-none d-sm-block" data-animate="fadeInUp" data-delay="200">
+                            <div>We Meet Every Tuesday Evening from 7:10pm to 8:10pm</div>
+                          </p>
+                          <hr />
+                          <p className="d-none d-sm-block" data-animate="fadeInUp" data-delay="200" style={{fontSize: ".8em"}}>
+                            <div>We are located:</div>
+                            <div>4501 Deer Valley Rd, Antioch, CA 94531</div>
+                            <div>Kaiser Permanente, Bldg 2, Conference Room 1B or 1C</div>
+                            <div>Zoom: <a href="https://zoom.us/j/711305684">711-305-684</a></div>
+                          </p>
+                          <p className="d-none d-sm-block" data-animate="fadeInUp" data-delay="200">
+                            {/* <a href="/contact-us/" className="button button-mlarge button-rounded">Contact Us</a> */}
+                            <a href="https://zoom.us/j/711305684" className="button button-mlarge button-rounded">Join Zoom Room</a>
+                          </p>
+                      </div>{/* /col */}
+                    </div>{/* /row */}
+                  </div>{/* /container */}
+                </div>
 
-  </div>
+              </div>
+              <div className="slider-arrow-left">
+                <i className="icon-angle-left"></i>
+              </div>
+              <div className="slider-arrow-right">
+                <i className="icon-angle-right"></i>
+              </div>
+            </div>
+            <a
+              href="/"
+              data-scrollto="#content"
+              data-offset="100"
+              className="dark one-page-arrow"
+            >
+              <i className="icon-angle-down infinite animated fadeInDown"></i>
+            </a>
+          </div>
+        </section>
+
+      </div>
+    )
+  }
 }
 
 export default Hero;
