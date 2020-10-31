1fec6686ed573f21f0693c810b2e2e53695d93e5
Derrick Leem <derricktleem@gmail.com>
2020-10-31 11:51:37 -0700

    Removed LinkedIn, Facebook, and phone #. Added wording.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import "./footer.css"
 
 const Footer = () => (
@@ -44,29 +44,29 @@ const Footer = () => (
               <abbr title="Phone Number">
                 <strong>Phone:</strong>
               </abbr>{" "}
-              (408) 483-5796                    
+              (408) 483-5796
             </div>
           </div>
-        </div>{/* col_one_third */}
-
+        </div>
+        {/* col_one_third */}
         <div className="col_one_third">
-          <div
-            className="widget clearfix"
-            style={{ marginBottom: `-20px` }}
-          >
+          <div className="widget clearfix" style={{ marginBottom: `-20px` }}>
             <div className="row">
               <div className="col-lg-6 clearfix bottommargin-sm">
                 <a
                   href="https://www.facebook.com/toastmasterscreatingcommunicators"
-                  target="_blank" rel="noopener noreferrer"
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className="social-icon si-dark si-colored si-facebook nobottommargin"
                   style={{ marginRight: `10px` }}
                 >
                   <i className="icon-facebook"></i>
                   <i className="icon-facebook"></i>
                 </a>
-                <a href="https://www.facebook.com/toastmasterscreatingcommunicators"
-                target="_blank" rel="noopener noreferrer"
+                <a
+                  href="https://www.facebook.com/toastmasterscreatingcommunicators"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   <small style={{ display: `block`, marginTop: `3px` }}>
                     <strong>Like us</strong>
@@ -77,9 +77,10 @@ const Footer = () => (
               </div>
             </div>
           </div>
-        </div> {/* col_one_third */}
-
+        </div>{" "}
+        {/* col_one_third */}
         <div className="col_one_third col_last">
+          <p align="right">Meeting Information / Directions</p>
           <iframe
             title="kaisermap"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5291.164402686531!2d-121.7757456584463!3d37.95039604917656!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808ff867ba2ed567%3A0xc7198e2e14850edf!2sKaiser%20Permanente%20Antioch!5e0!3m2!1sen!2sus!4v1604038493669!5m2!1sen!2sus"
@@ -98,49 +99,52 @@ const Footer = () => (
     <div id="copyrights">
       <div className="container clearfix">
         <div className="col_half">
-          Copyrights &copy; 2019 All Rights Reserved by <a href="/engineering">Toastmasters Engineering Team</a>.
+          Copyrights &copy; 2019 All Rights Reserved by{" "}
+          <a href="/engineering">Toastmasters Engineering Team</a>.
         </div>
 
         <div className="col_half col_last tright">
           <div className="fright clearfix">
             <a
               href="https://www.facebook.com/toastmasterscreatingcommunicators"
-              target="_blank" rel="noopener noreferrer"
+              target="_blank"
+              rel="noopener noreferrer"
               className="social-icon si-small si-borderless si-facebook"
             >
               <i className="icon-facebook"></i>
               <i className="icon-facebook"></i>
             </a>
 
-            <a
+            {/* <a
               href="https://www.linkedin.com/company-beta/18068202"
               target="_blank" rel="noopener noreferrer"
               className="social-icon si-small si-borderless si-linkedin"
             >
               <i className="icon-linkedin"></i>
               <i className="icon-linkedin"></i>
-            </a>
+            </a> */}
 
             <a
               href="https://www.meetup.com/Antioch-and-Pittsburg-CA-Public-Speakers-Meetup/"
-              target="_blank" rel="noopener noreferrer"
+              target="_blank"
+              rel="noopener noreferrer"
               className="social-icon si-small si-borderless si-pinterest"
             >
               <i className="icon-meetup"></i>
               <i className="icon-meetup"></i>
             </a>
 
-            <a
+            {/* <a
               href="https://twitter.com/AntiochTM?lang=en"
               target="_blank" rel="noopener noreferrer"
               className="social-icon si-small si-borderless si-twitter"
             >
               <i className="icon-twitter"></i>
               <i className="icon-twitter"></i>
-            </a>
+            </a> */}
           </div>
-          <div className="clear"></div>
-          <i className="icon-phone"></i> (408) 483-5796
+          {/* <div className="clear"></div>
+          <i className="icon-phone"></i> (408) 483-5796 */}
         </div>
       </div>
     </div>
@@ -148,4 +152,4 @@ const Footer = () => (
   </footer>
 )
 
-export default Footer;
+export default Footer
